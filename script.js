07de16a4927ded071202a21898ed6c518ab8255f
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-19 23:52:26 +1100

    Added single li with album cover, title, artistr and album

--- script.js
+++ script.js
@@ -69,11 +69,65 @@ console.log(searchBtn);
         return response.json()
     })
     .then(data => {
+        //Use returned list to populate a ul
         console.log(data);
+        populateList(data)
     })
     .catch(error => {
         console.log(error);
     });
     }
     }
-    
+
+    //Create a new track object and attach it to the list
+    class Track {
+        constructor(thisTrack) {
+            this.cover = thisTrack.album.images[0].url; //album cover
+            this.track = thisTrack.name;
+            this.artist = thisTrack.artists[0].name;
+            this.album = thisTrack.album.name;
+        }
+
+        makeList() {
+            //Create an li for each track and append it to the ul 
+            let resultsList = document.getElementById('results-list')
+            let trackContainer = document.createElement('li');
+            trackContainer.classList.add('track');
+            resultsList.appendChild(trackContainer);
+
+            //Create and append album cover
+            let thisCover = document.createElement('img')
+            thisCover.setAttribute('src', this.cover); //Sets the source attribute for image, the image url returned from Spotify
+            thisCover.setAttribute('alt', this.album); //Sets the alt attribute to the album name
+            trackContainer.appendChild(thisCover);
+
+            //Create and append track name
+            let thisTrackName = document.createElement('h2')
+            thisTrackName.innerHTML = this.track
+            trackContainer.appendChild(thisTrackName)
+
+            //Create and append artist name
+            let thisArtistName = document.createElement('h3')
+            thisArtistName.innerHTML = this.artist
+            trackContainer.appendChild(thisArtistName)
+
+            //Create and append artist name
+            let thisAlbumName = document.createElement('h3')
+            thisAlbumName.innerHTML = thisTrack.artists[0].name
+            trackContainer.appendChild(thisAlbumName)
+        }
+
+    }
+
+    function populateList(results) {
+
+        let eachTrack = results.tracks.items;
+        console.log(eachTrack);
+        let trackObj = eachTrack.forEach(function(track) {
+            let newTrack = new Track(track)
+            return newTrack.makeList();
+        });
+        console.log(trackObj);
+    }
+
+
