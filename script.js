2160a463d3f82afa5c335a62e2f1978f7b901534
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-21 15:42:18 +1100

    Added randomize

--- script.js
+++ script.js
@@ -1,83 +1,152 @@
+let questions = [
+    'favourite cereal?',
+    'favourite breakfast food?',
+    'favourite pizza topping/s?',
+    'favourite fruit?',
+    'favourite vegetable?',
+    'favourite dessert?',
+    'favourite food?',
+    'favourite cultural cuisine?',
+    'favourite cake?',
+    'favourite alcoholic drink?',
+    'favourite coffee order?',
+    'favourite cartoon cat?',
+    'favourite movie?',
+    'favourite country you’ve visited?',
+    'country you most want to visit?',
+    'favourite place to go with family?',
+    'favourite vacation you’ve taken?',
+    'favourite fictional place you\’d want to visit?',
+    'favourite thing about traveling?',
+    'favourite dinosaur?',
+    'favourite breed of dog?',
+    'favourite season?',
+    'your favourite flower?',
+    'favourite animal?',
+    'favourite native animal?',
+    'animal that you wish was native to your country?',
+    'favourite type of bear?',
+    'favourite reptile?',
+    'your favourite bird?',
+    'favourite way to spend a rainy day?',
+    'favourite way to spend a sunny day?',
+    'favourite sea creature?',
+    'favourite small mammal?',
+    'favourite big cat?',
+    'favourite wild animal?',
+    'favourite sport?',
+    'favourite childhood memory?',
+    'favourite board game?',
+    'favourite children’s show?',
+    'favourite toy as a child?',
+    'opinion of the best age of your life?',
+    "favourite Christmas present you've ever received?",
+    'favourite superhero?',
+    'favourite video game?',
+    'favourite quote?',
+    'favourite department store?',
+    'favourite place to shop?',
+    'favourite store in the mall?',
+    'favourite perfume/cologne?',
+    'favourite occasion to dress up for?',
+    'favourite hairstyle?',
+    'favourite outfit you have?',
+    'favourite soap scent?',
+    'favourite article of clothing?',
+    'favourite luxury brand?',
+    'favourite brand of toilet paper?',
+    'favourite candle scent?',
+    'favourite extracurricular activity?',
+    'favourite day of the week?',
+    'favourite holiday?',
+    'favourite kind of house?',
+    'favourite baby boy name?',
+    'favourite baby girl name?',
+    'favourite celebrity?',
+    'favourite thing you’ve done in the last 24 hours?',
+    'favourite place to meet up with friends?',
+    'favourite hobby?',
+    'favourite way to cheer you up?',
+    'favourite hobby?',
+    'favourite kind of gift to receive?',
+    'favourite crafty thing to make?',
+    'favourite way to relax?',
+    'favourite musical',
+    'recommended way to solve a dispute',
+    'solution to climate change'
+    ]
 
-/*
-let accessToken;
-let clientId = '16816dc28118429aad94cb9c64ee01c2'; // Your client id
-let clientSecret = 'd34aa51bb8b6412a9a7dbd7eb776e585'; // Your secret
-getToken();
+    let freshQuestions = questions.slice();
 
-//Change to a post request
-function getToken() {
-    fetch('https://accounts.spotify.com/api/token', {
-       method: 'POST',
-       headers: {
-           'Content-Type' : 'application/x-www-form-urlencoded', 
-           'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
-       },
-       body: 'grant_type=client_credentials'
-       })
-   .then((response) => {
-       return response.json()
-   })
-   .then((data) => {
-       JSON.stringify(data);
-       accessToken = data.access_token;
-       console.log(accessToken)
-       searchBtn.addEventListener('submit', function(event){
-           event.preventDefault();
-           let searchVal = document.getElementById('search').value;
-           searchQuery(searchVal, accessToken);
-       });
-   })
-   .catch((error) => {
-       console.log(error);
-   })
-   }
-   */
+function generateRandomQuestion() {
+    console.log('clicked');
+    //pick a random value from the array of fresh questions
+    let random = Math.floor(Math.random() * freshQuestions.length);
+    
+    //capture the random element chosen
+    let thisElement = freshQuestions[random];
 
+    //remove it so its not chosen again
+    freshQuestions.splice(random, 1)
+
+    return thisElement
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    setInputText();
+})
+
+function setInputText() {
+    let inputText = document.getElementById('search').setAttribute('placeholder', `What is your ${generateRandomQuestion()}`);
+}
+  
 let searchBtn = document.getElementById('spotify-form')
 
 searchBtn.addEventListener('submit', function(event){
     event.preventDefault();
+    document.getElementById("results-list").innerHTML = ""; //Remove all previous values before loading new ones
     let searchVal = document.getElementById('search').value;
     searchQuery(searchVal, accessToken);
+    document.getElementById('search').value = `What is your ${generateRandomQuestion()}`
+    document.getElementById('search').style.color = '#fff'
 });
 
 //Using access token in getdata.js
 function searchQuery(searchValue, token) {
 
-const form = document.getElementById('spotify-form');
-console.log(form)
-//   const log = document.getElementById('log');
-//   form.addEventListener('submit', logSubmit);
+    const form = document.getElementById('spotify-form');
+    console.log(form)
+    //   const log = document.getElementById('log');
+    //   form.addEventListener('submit', logSubmit);
 
-let searchBtn = document.getElementById('spotify-form');
-console.log(searchBtn);
+    let searchBtn = document.getElementById('spotify-form');
+    console.log(searchBtn);
 
     console.log(token);
     if (token != undefined) {
-    //replace spaces with + signs as thats what url accepts for string
-    let concatString = searchValue.split(' ').join('+');
-    console.log(searchValue, concatString);
-    fetch(`https://api.spotify.com/v1/search?q=${searchValue}&offset=0&limit=20&type=track`, {
-        headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token
-        }
-     })
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        //Use returned list to populate a ul
-        console.log(data);
-        populateList(data)
-    })
-    .catch(error => {
-        console.log(error);
-    });
+        //replace spaces with + signs as thats what url accepts for string
+        let concatString = searchValue.split(' ').join('+');
+        console.log(searchValue, concatString);
+        fetch(`https://api.spotify.com/v1/search?q=${searchValue}&offset=0&limit=20&type=track`, {
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+            }
+            })
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            //Use returned list to populate a ul
+            console.log(data);
+            populateList(data)
+        })
+        .catch(error => {
+            console.log(error);
+        });
     }
-    }
+}
 
     //Create a new track object and attach it to the list
     class Track {
@@ -127,7 +196,6 @@ console.log(searchBtn);
     }
 
     function populateList(results) {
-
         let eachTrack = results.tracks.items;
         console.log(eachTrack);
         eachTrack.forEach(function(track) {
@@ -135,5 +203,3 @@ console.log(searchBtn);
              newTrack.makeList();
         });
     }
-
-
